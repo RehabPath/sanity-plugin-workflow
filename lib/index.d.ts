c35d64a95b90bc89932bd3cc40b96eb29ee5bcea
Eduardo Landeros <elanderos318@gmail.com>
2025-01-09 12:12:30 -0600

    run npm run build

---
+++ lib/index.d.ts
@@ -0,0 +1,20 @@
+import {Plugin as Plugin_2} from 'sanity'
+
+declare type State = {
+  id: string
+  transitions: string[]
+  title: string
+  roles?: string[]
+  requireAssignment?: boolean
+  requireValidation?: boolean
+  color?: 'primary' | 'success' | 'warning' | 'danger'
+}
+
+export declare const workflow: Plugin_2<WorkflowConfig>
+
+declare type WorkflowConfig = {
+  schemaTypes: string[]
+  states?: State[]
+}
+
+export {}
